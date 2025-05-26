47f85c932cead336585fcaaabc6474893703b97b
Buty06 <mecagoenlashormigas@gmail.com>
2025-05-26 16:56:37 -0400

    Terminando la lógica del input y procediendo al código QR

--- src/components/QR.tsx
+++ src/components/QR.tsx
@@ -1,7 +1,29 @@
+import { useState } from "react";
+
 export const QR = () => {
+  const [urlName, setUrlName] = useState("");
+
+  const handlerQRGenerator = (event: React.MouseEvent) => {
+    event.preventDefault();
+
+    const inputElement = document.getElementById("input") as HTMLInputElement;
+    const value: string = inputElement.value;
+
+    if (!value.includes("https://") || !value.includes("http://")) {
+      setUrlName("Tiene que ser una url el valor ingresado");
+    } else {
+      setUrlName(value);
+    }
+  };
+
   return (
     <section>
-      <h3>2</h3>
+      <form>
+        <input type="text" id="input" />
+        <button onClick={handlerQRGenerator}>Push Me</button>
+      </form>
+
+      <p>{urlName}</p>
     </section>
   );
 };
